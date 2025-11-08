18b5746aa812048e24bedc5db34e3c68ed432b35
ALMOK\osame <osameh314@gmail.com>
2025-11-08 09:27:56 +0100

    task5

---
+++ task5.py
@@ -0,0 +1,6 @@
+""" Compute the sum and product of two numbers input by the user, displaying the results in a single print() statement using the sep argument."""
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+product_result = num1 * num2
+print("Sum:", sum_result, "Product:", product_result, sep=" | ")
