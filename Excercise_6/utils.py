35ba1deeba454b8ca69894c3e915cc1b3ccc5ef6
ALMOK\osame <osameh314@gmail.com>
2025-11-29 09:52:11 +0100

    .

---
+++ Excercise_6/utils.py
@@ -0,0 +1,6 @@
+"""  create a function that calculates the area of a rectangle and one that calculates the perimeter of a rectangle."""
+def rectangle_area(length, width):
+    return length * width
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
+
